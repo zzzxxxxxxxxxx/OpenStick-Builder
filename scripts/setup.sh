76c7aa749510e1a30407637bbcb2ba7c20ad9ee4
zzxxx <120317497+zzzxxxxxxxxxx@users.noreply.github.com>
2026-02-03 22:35:11 +0800

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -17,7 +17,7 @@ apt install -qqy --no-install-recommends \
     dnsmasq \
     hostapd \
     iptables \
-    libconfig9 \
+    libconfig11 \
     locales \
     modemmanager \
     netcat-traditional \
