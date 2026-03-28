a1c3372779e5c655c2fb20500055dc117ee69a54
zzxxx <120317497+zzzxxxxxxxxxx@users.noreply.github.com>
2026-03-28 14:10:18 +0800

    Update debootstrap.sh

--- scripts/debootstrap.sh
+++ scripts/debootstrap.sh
@@ -55,7 +55,7 @@ SUBSYSTEM=="net", ACTION=="add|change|move", ENV{DEVTYPE}=="gadget", ENV{NM_UNMA
 EOF
 
 # install kernel
-wget -O - http://mirror.postmarketos.org/postmarketos/v24.06/aarch64/linux-postmarketos-qcom-msm8916-6.6-r5.apk \
+wget -O - https://mirror.postmarketos.org/postmarketos/main/aarch64/linux-postmarketos-qcom-msm8916-6.12.1-r3.apk \
     | tar xkzf - -C ${CHROOT} --exclude=.PKGINFO --exclude=.SIGN* 2>/dev/null
 
 mkdir -p ${CHROOT}/boot/extlinux
